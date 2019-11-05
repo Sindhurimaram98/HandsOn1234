8cdb8ba126f6d4821ecb7816f90e1d727ed2a0a0
Training_H2A.06.17 <Training_h2a.06.17@accenture.com>
2019-11-05 13:21:34 +0530

    exam handson

--- src/test/java/stepdefhandson/TestMeApp.java
+++ src/test/java/stepdefhandson/TestMeApp.java
@@ -19,30 +19,30 @@ public class TestMeApp {
 		
 		System.setProperty("webdriver.chrome.driver","C:\\MyDrivers\\chromedriver_win32\\chromedriver.exe");
 		d=new ChromeDriver();
-		d.get("http://github.com/");
-//		d.get("http://demowebshop.tricentis.com/");
+		//d.get("http://github.com/");
+		d.get("http://demowebshop.tricentis.com/");
 		d.manage().window().maximize();
 		
 	}
 	@When("user enters {string} and {string}")
 	public void user_enters_and(String username,String password)
 	{
-//		d.findElement(By.linkText("Log in")).click();
-//		d.findElement(By.xpath("//input[@name='Email']")).sendKeys(username);
-//		d.findElement(By.xpath("//input[@name='Password']")).sendKeys(password);
-//		d.findElement(By.xpath("//input[@value='Log in']")).click();
+	    d.findElement(By.linkText("Log in")).click();
+		d.findElement(By.xpath("//input[@name='Email']")).sendKeys(username);
+		d.findElement(By.xpath("//input[@name='Password']")).sendKeys(password);
+		d.findElement(By.xpath("//input[@value='Log in']")).click();
 	}
 	@Then("Message Login successfully")
 	public void message_Login_successfully() {
-//		String act=d.getTitle();
-//		String exp="Demo Web Shop";
-		//if(act.equals(exp))
-		//{
-		//	System.out.println("login succesfull");
-		//}
-//		Assert.assertEquals(exp, act);
-//		System.out.println("login succesfull");
-		//d.close();
+		String act=d.getTitle();
+		String exp="Demo Web Shop";
+		if(act.equals(exp))
+		{
+			System.out.println("login succesfull");
+		}
+		Assert.assertEquals(exp, act);
+		System.out.println("login is succesfull");
+		d.close();
 		
 		
 		
